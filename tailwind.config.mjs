4caa7533cfd786c78d1d6ae1b1d6f0e129f96f37
TheDaidor2001 <danielcastillo1552001@hotmail.com>
2024-04-04 11:17:39 +0100

    proyectos y titulacion añadida en español

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			colors: {
+				"secondary": "#cbff4d"
+			},
 			backgroundImage: {
 				'main': "url('/images/cara.webp')",
 			},
@@ -10,23 +13,23 @@ export default {
 			animation: {
 				"background-shine": "background-shine 2s linear infinite",
 				'infinite-scroll': 'infinite-scroll 15s linear infinite',
-			  },
+			},
 			keyframes: {
 				"background-shine": {
-				  "from": {
-					"backgroundPosition": "0 0"
-				  },
-				  "to": {
-					"backgroundPosition": "-200% 0"
-				  }
+					"from": {
+						"backgroundPosition": "0 0"
+					},
+					"to": {
+						"backgroundPosition": "-200% 0"
+					}
 				},
 				'infinite-scroll': {
 					from: { transform: 'translateX(0)' },
 					to: { transform: 'translateX(-100%)' },
 				}
-			  }
+			}
 		},
 	},
-	
+
 	plugins: [],
 }
