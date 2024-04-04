ca1aa094c9853797565f6600201420c15d31a78f
TheDaidor2001 <danielcastillo1552001@hotmail.com>
2024-04-04 11:31:45 +0100

    proyectos y titulos page en ingles

---
+++ src/data/proyectos.ts
@@ -0,0 +1,53 @@
+export const PROYECTOS = [
+    {
+        title: "Gartoll Web",
+        description:
+            "Una web corporativa para una empresa de Seguridad Privada.",
+        technologies: ["astro", "tailwindcss", "html5", "css", "typescript"],
+        image: "/images/gartoll-desktop.webp",
+        image2: "/images/gartoll-mobile.webp",
+        link: "https://www.gartoll-seguridad.com",
+    },
+    {
+        title: "Teslo Shop",
+        description: "Un ecommerce completo de ropa con inicio de sesión",
+        technologies: [
+            "nextjs",
+            "tailwindcss",
+            "html5",
+            "css",
+            "typescript",
+            "postgresql",
+        ],
+        image: "/images/teslo-desktop.webp",
+        image2: "/images/teslo-mobile.webp",
+        link: "https://tesla-shop-daniel.vercel.app/",
+    },
+];
+
+export const PROYECTOSENGLISH = [
+    {
+        title: "Gartoll Web",
+        description:
+            "A corporate website for a Private Security company",
+        technologies: ["astro", "tailwindcss", "html5", "css", "typescript"],
+        image: "/images/gartoll-desktop.webp",
+        image2: "/images/gartoll-mobile.webp",
+        link: "https://www.gartoll-seguridad.com",
+    },
+    {
+        title: "Teslo Shop",
+        description: "A complete clothing ecommerce with login and payments",
+        technologies: [
+            "nextjs",
+            "tailwindcss",
+            "html5",
+            "css",
+            "typescript",
+            "postgresql",
+        ],
+        image: "/images/teslo-desktop.webp",
+        image2: "/images/teslo-mobile.webp",
+        link: "https://tesla-shop-daniel.vercel.app/",
+    },
+];
---
+++ src/data/titulos.ts
@@ -0,0 +1,37 @@
+export const TITULOS = [
+    {
+        title: "Curso de Javascript completo",
+        image: "/images/c-js.webp",
+    },
+    {
+        title: "Curso CSS",
+        image: "/images/c-css.webp",
+    },
+    {
+        title: "Curso Next- Framework de ReactJS",
+        image: "/images/c-next.webp",
+    },
+    {
+        title: "Curso VueJS",
+        image: "/images/c-vue.webp",
+    },
+];
+
+export const TITULOSENGLISH = [
+    {
+        title: "Complete JavaScript course",
+        image: "/images/c-js.webp",
+    },
+    {
+        title: "CSS course",
+        image: "/images/c-css.webp",
+    },
+    {
+        title: "Next.js - ReactJS Framework Course",
+        image: "/images/c-next.webp",
+    },
+    {
+        title: "VueJS course",
+        image: "/images/c-vue.webp",
+    },
+];
